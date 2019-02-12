b9d2343e7c2db7ad4addd7c548832a7dc83bec9e
XueSarahSong <songxy2@mcmaster.ca>
2019-02-11 23:10:58 -0500

    clean up to make it easier to read

--- Project_borealis_sex_determination/scripts/clustering_with_orthoGeneOnly.R
+++ Project_borealis_sex_determination/scripts/clustering_with_orthoGeneOnly.R
@@ -6,44 +6,40 @@ library(factoextra) # clustering algorithms & visualization
 input_file_path <- file.path("/Users/evanslab/Desktop",
                              "Xue_master_projects","Project_borealis_sex_determination/Analysis",
                              "sample_clustering_analysis")
-
 output_file_path <- input_file_path
-
 input_file <- paste(input_file_path, 
-                    "identify_by_borealisGonad_DEgene/borealisGonadDEgene_4xBothSex_tropTran_blastnOut.tsv", 
+                    "identify_by_borealisGonad_DEgene",
+                    "borealisGonadDEgene_4xBothSex_tropTran_blastnOut.tsv", 
                     sep="/")
 
 input_data <- read_tsv(input_file, col_names = FALSE)
 
+#select only the top trop hit for each borealise transcript
 ortho_list <- input_data%>%
   group_by(X1)%>%
   arrange(X11, .by_group = TRUE)%>%
-  top_n(1,X11)%>%
-  top_n(1,X12)
-
-rm(input_data)
-
-ortho_trans <- ortho_list %>% 
+  top_n(1,X11)%>% #select the one with the lowest pvalue
+  top_n(1,X12)%>% #if have ties for pvalue, select by bitscore
   ungroup() %>% 
   select(X2) %>% 
   rename("Transcript_name"="X2")%>%
   unique()
 
+rm(input_data)
+
+#read in the expression file
 expression_file = paste(input_file_path,
                         "PCA/tropicalis_gonad.isoform.TMM.EXPR.matrix",
                         sep="/")
-
 expression_data <- read_tsv(expression_file)
 
+#select transcript that mapped to the borealise sex-biased gene
 ortho_expression <- expression_data %>%
-  inner_join(ortho_trans, by=("Transcript_name"))%>%
+  inner_join(ortho_list, by=("Transcript_name"))%>%
   column_to_rownames("Transcript_name")
-
 ortho_expression <- ortho_expression[rowSums(ortho_expression)>0,]
 ortho_expression <- t(ortho_expression)
 ortho_expression <- scale(ortho_expression)
-
-head(ortho_expression)
 
 ####### Check scaling
 #The below indicated that scaling did scale the data to have a mean of 0 and sd of 1
@@ -53,6 +49,7 @@ scaling_check <- as.data.frame (t(ortho_expression)) %>%
   group_by(Transcript_name)%>%
   mutate (Mean = mean(expression_value), SD =sd(expression_value) )
 
+rm(scaling_check)
 
 ###### PCA
 project_pca <- prcomp(ortho_expression, retx=TRUE, center = FALSE, scale. = FALSE)
@@ -65,7 +62,6 @@ pca.var <- project_pca$sdev^2
 pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
 
 ## now make a fancy looking plot that shows the PCs and the variation:
-
 pca_plot <- data.frame(Sample=rownames(project_pca$x),
                        X=project_pca$x[,1],
                        Y=project_pca$x[,2])
@@ -82,9 +78,9 @@ ggplot(data=pca_plot, aes(x=X, y=Y, label=Sample)) +
   geom_vline(mapping = NULL, data = NULL, xintercept = 0,
              na.rm = FALSE, show.legend = NA)
 
-#kmeans
-set.seed(20)
-expCluster <- kmeans(ortho_expression, 2, nstart = 20)
+##### kmeans
+set.seed(200)
+expCluster <- kmeans(ortho_expression, 2, nstart = 200)
 expCluster$cluster
 
 fviz_cluster(expCluster, data = ortho_expression)
