6e64d1167b7db2b43a3612e2af6a7eba0823f660
XueSarahSong <songxy2@mcmaster.ca>
2019-02-25 18:05:33 -0500

    DTU script

---
+++ Project_borealis_sexual_antagonism/Scripts/DTU_supertranscriptome.R
@@ -0,0 +1,81 @@
+
+####import data####
+library(tximport)
+quant_files <- file.path("Project_borealis_sexual_antagonism/data/supertranscriptome/counts_salmon/quants", 
+                         list.files("quants"), "quant.sf")
+
+samples <- c("BJE3896_dad_quant", 
+             "BJE3929_boy_quant",
+             "BJE4017_boy_quant", 
+             "BJE4039_boy_quant",
+             "BJE3897_mom_quant", 
+             "BJE4072_girl_quant",
+             "BJE4009_girl_quant",
+             "BJE4082_girl_quant")
+
+names(quant_files) <- samples
+
+txi <- tximport(files, type="salmon", txOut=TRUE,
+                countsFromAbundance="scaledTPM")
+cts <- txi$counts
+cts <- cts[rowSums(cts) > 0,]
+
+
+#
+library(rnaseqDTU)
+csv.dir <- system.file("extdata", package="rnaseqDTU")
+
+samps <- read.csv(file.path(csv.dir, "samples.csv"))
+head(samps)
+samps$condition <- factor(samps$condition)
+table(samps$condition)
+files <- file.path("/home/xue/borealis_transcriptome/borealis_denovo_transcriptome_dec2018/analysis/supertranscriptome/count_salmon", 
+                   samps$sample_id, "quant.sf")
+names(files) <- samps$sample_id
+head(files)
+
+library(DEXSeq)
+sample.data <- DRIMSeq::samples(d)
+count.data <- round(as.matrix(counts(d)[,-c(1:2)]))
+dxd <- DEXSeqDataSet(countData=count.data,
+                     sampleData=sample.data,
+                     design=~sample + exon + condition:exon,
+                     featureID=counts(d)$feature_id,
+                     groupID=counts(d)$gene_id)
+
+system.time({
+  dxd <- estimateSizeFactors(dxd)
+  dxd <- estimateDispersions(dxd, quiet=TRUE)
+  dxd <- testForDEU(dxd, reducedModel=~sample + exon)
+})
+
+dxr <- DEXSeqResults(dxd, independentFiltering=FALSE)
+qval <- perGeneQValue(dxr)
+dxr.g <- data.frame(gene=names(qval),qval)
+
+
+columns <- c("featureID","groupID","pvalue")
+dxr <- as.data.frame(dxr[,columns])
+head(dxr)
+
+
+#######StageR
+ata(dex_tables)
+
+library(stageR)
+strp <- function(x) substr(x,1,15)
+pConfirmation <- matrix(dxr$pvalue,ncol=1)
+dimnames(pConfirmation) <- list(strp(dxr$featureID),"transcript")
+pScreen <- qval
+names(pScreen) <- strp(names(pScreen))
+tx2gene <- as.data.frame(dxr[,c("featureID", "groupID")])
+for (i in 1:2) tx2gene[,i] <- strp(tx2gene[,i])
+
+stageRObj <- stageRTx(pScreen=pScreen, pConfirmation=pConfirmation,
+                      pScreenAdjusted=TRUE, tx2gene=tx2gene)
+stageRObj <- stageWiseAdjustment(stageRObj, method="dtu", alpha=0.05)
+suppressWarnings({
+  dex.padj <- getAdjustedPValues(stageRObj, order=FALSE,
+                                 onlySignificantGenes=TRUE)
+})
+head(dex.padj)
