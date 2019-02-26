0e9c4611d87cbb1ca0499078a9dbfc071563bfd2
XueSarahSong <songxy2@mcmaster.ca>
2019-02-26 18:56:31 -0500

    dtu script

--- Project_borealis_sexual_antagonism/Scripts/DTU_supertranscriptome.R
+++ Project_borealis_sexual_antagonism/Scripts/DTU_supertranscriptome.R
@@ -2,38 +2,55 @@
 ####import data####
 library(tximport)
 quant_files <- file.path("Project_borealis_sexual_antagonism/data/supertranscriptome/counts_salmon/quants", 
-                         list.files("quants"), "quant.sf")
-
-samples <- c("BJE3896_dad_quant", 
+                         list.files("Project_borealis_sexual_antagonism/data/supertranscriptome/counts_salmon/quants"), "quant.sf")
+samples <- c("BJE3896_dad_quant",
+             "BJE3897_mom_quant",
              "BJE3929_boy_quant",
+             "BJE4009_girl_quant",
              "BJE4017_boy_quant", 
              "BJE4039_boy_quant",
-             "BJE3897_mom_quant", 
              "BJE4072_girl_quant",
-             "BJE4009_girl_quant",
              "BJE4082_girl_quant")
 
 names(quant_files) <- samples
 
-txi <- tximport(files, type="salmon", txOut=TRUE,
+txi <- tximport(quant_files, type="salmon", txOut=TRUE,
                 countsFromAbundance="scaledTPM")
 cts <- txi$counts
 cts <- cts[rowSums(cts) > 0,]
 
 
-#
-library(rnaseqDTU)
-csv.dir <- system.file("extdata", package="rnaseqDTU")
-
-samps <- read.csv(file.path(csv.dir, "samples.csv"))
-head(samps)
+#load in the condition information
+samps <- read.csv(file.path("Project_borealis_sexual_antagonism/data/supertranscriptome/DTU_analysis", "sample_condition.csv"))
 samps$condition <- factor(samps$condition)
 table(samps$condition)
-files <- file.path("/home/xue/borealis_transcriptome/borealis_denovo_transcriptome_dec2018/analysis/supertranscriptome/count_salmon", 
-                   samps$sample_id, "quant.sf")
-names(files) <- samps$sample_id
-head(files)
 
+#transcript to gene mapping
+library(GenomicFeatures)
+gtf <- "Project_borealis_sexual_antagonism/data/supertranscriptome/supertranscriptome_fasta/borealis_superTrans.gtf"
+txdb.filename <- "borealis_superTrans_gtf.sqlite"
+txdb <- makeTxDbFromGFF(gtf)
+saveDb(txdb, txdb.filename)
+txdb <- loadDb(txdb.filename)
+txdf <- select(txdb, keys(txdb, "GENEID"), "TXNAME", "GENEID")
+tab <- table(txdf$GENEID)
+txdf$ntx <- tab[match(txdf$GENEID, names(tab))]
+
+#build a DRIMSeq object
+cts[1:3,1:3]
+range(colSums(cts)/1e6)
+all(rownames(cts) %in% txdf$TXNAME)
+txdf <- txdf[match(rownames(cts),txdf$TXNAME),]
+all(rownames(cts) == txdf$TXNAME)
+
+counts <- data.frame(gene_id=txdf$GENEID,
+                     feature_id=txdf$TXNAME,
+                     cts)
+library(DRIMSeq)
+d <- dmDSdata(counts=counts, samples=samps)
+d
+
+#build a DEXSeq 
 library(DEXSeq)
 sample.data <- DRIMSeq::samples(d)
 count.data <- round(as.matrix(counts(d)[,-c(1:2)]))
